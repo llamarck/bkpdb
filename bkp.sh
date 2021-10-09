ef4f73ed3fc8fc0267931434aac384624d0c07f1
Leonis Lamarck <lamarck.ti@gmail.com>
2021-10-08 22:24:10 -0300

    Adding number of days

--- bkp.sh
+++ bkp.sh
@@ -4,3 +4,5 @@ backup_dir="/backup/"
 #String to append to the name of the backup files.
 backup_date=`date +%d-%m-%Y`
 PGBINDIR="/usr/pgsql-9.6/bin"
+#Numbers of days you want to keep copie of your databases
+number_of_days=3
